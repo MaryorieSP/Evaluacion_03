27a280e41caff33cae6ef17a112954d6612e81e9
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-07-24 17:32:04 +0000

    e

--- src/main/java/com/example/demo/InstrumentoRepository.java
+++ src/main/java/com/example/demo/InstrumentoRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(path="/api/ec3")
 @RepositoryRestResource(collectionResourceRel = "instrumentos",path="instrumentos")
 public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento,Integer>,CrudRepository<Instrumento,Integer>{
 
