9bbf77e316f30fd55078573b008c5277b11ccfba
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-07-24 16:55:38 +0000

    cuatro

--- src/main/java/com/example/demo/CategoriaRepository.java
+++ src/main/java/com/example/demo/CategoriaRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
-public class CategoriaRepository {
-    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+
+
+@RepositoryRestResource(collectionResourceRel = "categorias",path = "categorias")
+public interface CategoriaRepository extends PagingAndSortingRepository<Categoria,Integer>,CrudRepository<Categoria,Integer> {
+    List<Categoria> findByNombre(@Param("nombre")String nombre);
 }
